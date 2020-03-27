0c68dd52bc11a677f9485bf9e17dd58f332399a2
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-27 11:33:05 -0600

    215. Generar un nombre único a la imagen

--- src/classes/file-system.ts
+++ src/classes/file-system.ts
@@ -2,14 +2,20 @@ import path from 'path';
 import fs from 'fs';
 
 import { UploadedFile } from 'express-fileupload';
+import uniqid from 'uniqid';
 
 export default class FileSystem {
 
     static saveTempImage(file: UploadedFile, userId: string) {
+
         const path = this.createUserFolder(userId);
+        const filename = this.generateUniqueFilename(file.name);
+
+        console.log(filename);
+
     }
 
-    static createUserFolder(userId: string) {
+    private static createUserFolder(userId: string): string {
 
         const userPath = path.resolve(__dirname, '../uploads', userId);
         const userPathTemp = userPath + '/temp';
@@ -22,6 +28,17 @@ export default class FileSystem {
         }
 
         return userPathTemp;
+
+    }
+
+    private static generateUniqueFilename(originalFilename: string) {
+
+        const originalFilenameArr = originalFilename.split('.');
+        const extension = originalFilenameArr[originalFilenameArr.length - 1];
+
+        const uniqueId = uniqid();
+
+        return `${uniqueId}.${extension}`;
 
     }
 
