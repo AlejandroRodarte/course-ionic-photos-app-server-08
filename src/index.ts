722ace4397a5c0abbc4d3f1a0c803b5e163345ef
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-26 20:37:24 -0600

    198. Crear un usuario de FotosGram

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
+import bodyParser from 'body-parser';
+
 import Server from './classes/server';
 
 import userRoutes from './routes/usuario';
@@ -5,6 +7,9 @@ import userRoutes from './routes/usuario';
 import './db/mongoose';
 
 const server = new Server();
+
+server.app.use(bodyParser.urlencoded({ extended: true }));
+server.app.use(bodyParser.json());
 
 server.app.use('/user', userRoutes);
 
--- src/routes/usuario.ts
+++ src/routes/usuario.ts
@@ -1,13 +1,35 @@
 import { Router, Request, Response } from 'express';
 
+import Usuario from '../models/usuario';
+
 const userRoutes = Router();
 
-userRoutes.get('/prueba', (req: Request, res: Response) => {
+userRoutes.post('/create', async (req: Request, res: Response) => {
 
-    res.json({
+    const { nombre, email, password, avatar = undefined } = req.body;
+
+    const user = {
+        nombre,
+        email,
+        password,
+        avatar
+    };
+
+    const userDoc = new Usuario(user);
+
+    try {
+        await userDoc.save();
+    } catch (err) {
+        return res.status(400).send({
+            ok: false,
+            err
+        });
+    }
+
+    return res.json({
         ok: true,
-        mensaje: 'Todo bien'
-    })
+        user: userDoc
+    });
 
 });
 
