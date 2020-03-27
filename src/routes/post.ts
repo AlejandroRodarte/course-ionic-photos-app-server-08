46d252e090dec2eb56e3abadd89397cdfdf73737
Alejandro Rodarte <alejandrorodarte1@gmail.com>
2020-03-27 10:25:57 -0600

    211. Servicio para obtener los POSTs de forma paginada

--- src/routes/post.ts
+++ src/routes/post.ts
@@ -1,8 +1,32 @@
-import { Router, Response } from 'express';
+import { Router, Response, Request } from 'express';
 import auth from '../middlewares/auth';
 import Post from '../models/post';
 
 const postRoutes = Router();
+
+postRoutes.get('/', async (req: Request, res: Response) => {
+
+    const pagina = req.query.pagina ? +req.query.pagina : 1;
+    const skip = 10 * (pagina - 1);
+
+    const posts = 
+        await Post
+                .find({})
+                .sort({ _id: -1 })
+                .skip(skip)
+                .limit(10)
+                .populate({
+                    path: 'usuario',
+                    select: '-password'
+                });
+
+    return res.json({
+        ok: true,
+        pagina,
+        posts
+    });
+
+});
 
 postRoutes.post('/', auth, async (req: any, res: Response) => {
 
